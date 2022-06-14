6f32b65cfbb633f3d6ad68392eff79fef36b07a7
hshk1004 <hshk_1004@naver.com>
2022-06-15 04:51:59 +0900

    html파일 디렉토리 변경 및 루트 추적

--- src/input_htmls/input_json_html.ts
+++ src/input_htmls/input_json_html.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import path from "path";
 
 const router = express.Router();
+const route_html = path.resolve(__dirname, "../../htmls", "input_html.html")
 
 router.use(function(req,res,next){
     console.log('메인 페이지 입니다.');
-    res.sendFile(__dirname + '/input_html.html');
+    res.sendFile(route_html);
 });
 
 module.exports = router;

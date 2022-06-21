df34ab81ee845225f2205de6467e3607eca570d7
hshk1004 <hshk_1004@naver.com>
2022-06-21 21:55:54 +0900

    오류 해결을 위해 헤로쿠리포 리셋후 재 업로드 위한 변경

--- dfout/db.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const db = {
-    host: 'eyvqcfxf5reja3nv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    user: 's6emirlp7oqk0vkr',
-    password: 'i8lnbfrc8jehaauz',
-    database: 'i2e5wpobc99ozlzq'
-};
-module.exports = db;
